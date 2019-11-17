992aff8745c0ec6dc869f3ff9e1122f353583fa4
liucancan <916215796@qq.com>
2019-11-17 08:18:04 +0800

    no message

--- src/main/java/com/xdclass/shop/controller/TestController.java
+++ src/main/java/com/xdclass/shop/controller/TestController.java
@@ -26,7 +26,7 @@ public class TestController {
 
     @GetMapping("/test")
     public void test(String info) throws Exception {
-        Message message = new Message("tx-mq-TOPIC", "Tag1", "12345", "rocketmq测试成功".getBytes());
+        Message message = new Message("BenchmarkTest", "Tag1", "12345", "rocketmq测试成功".getBytes());
         // 这里用到了这个mq的异步处理，类似ajax，可以得到发送到mq的情况，并做相应的处理
         //不过要注意的是这个是异步的
         producer.send(message, new SendCallback() {
